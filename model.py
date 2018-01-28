bab35eb3e01838ca6f72f1f93020ef26e2496e6c
Billy Ng <billy@billyng.net>
2018-01-27 23:51:30 -0500

    More progress

--- model.py
+++ model.py
@@ -3,7 +3,7 @@
 from pyoms import orderManager
 
 om = orderManager.OrderManager()
-no = om.create_order()
+
 
 orderDetails = {'symbol': 'IBM', 'side': 'Buy', 'quantity': 100, 'price': 245.09, 'destination': 'XNYS'}
 om.book.orders[no].set_properties_dict(orderDetails)
